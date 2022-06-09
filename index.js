5443253eb3d57966ef8ff879ce96f1461dbd296a
Olga Nadongo <winnienadongo@gmail.com>
2022-06-09 17:01:25 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // code your solution here
+
+function saturdayFun(activity="roller-skate"){
+    /*const activity="roller-skate"*/
+    return `This Saturday, I want to ${activity}!`
+
+}
+saturdayFun("bathe my dog");
+
+
+function mondayWork(doSomething="go to the office"){
+     return `This Monday, I will ${doSomething}.`;
+}
+mondayWork("go to class");
+
+   /* let result= wrapAdjective("*")
+    let emphatic= result("a hard worker")
+  function result() {
+     return `You are ${result} ${emphatic} ${result}!`
+  }*/
+  
+  /*function wrapAdjective(msg="You are"){
+
+     return function (wrapper= "*", otherMsg="a hard worker"){
+       
+        return `${msg} ${wrapper} ${otherMsg}`;
+  };
+}
+outer("You are")("||","a dedicated programmer");*/
+function wrapAdjective(wrapper="*"){
+  const part1="You are"
+   
+         return function(quality="special"){
+            
+            return `${part1} ${wrapper}${quality}${wrapper}!`
+
+         };
+    
+};
